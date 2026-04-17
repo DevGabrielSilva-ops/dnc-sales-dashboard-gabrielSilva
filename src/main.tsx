fc23d58db5bb7687c383218d35388b76ebce62a0
Gabriel Silva <dev13.gabriel@gmail.com>
2026-04-17 12:51:33 -0300

    Finalizando alteração

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import { temaClaro,temaEscuro,GlobalStyle} from './styles'
+import { temaEscuro,GlobalStyle} from './styles'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
