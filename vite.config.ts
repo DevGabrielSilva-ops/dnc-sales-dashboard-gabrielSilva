11f626052df925b6f74ab5e26716b41dd01ead39
Gabriel Silva <dev13.gabriel@gmail.com>
2026-04-17 14:12:10 -0300

    Feat: Adicionando material-ui

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+      include: ['@emotion/styled'],
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
