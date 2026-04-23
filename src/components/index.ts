6eb0c65210663e2a94b099c2a159b7b490bc2f43
Gabriel Silva <dev13.gabriel@gmail.com>
2026-04-23 10:44:01 -0300

    Achei o erro

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export * from './BannerImagem'
-export {default as ListaAvatar} from './avatarList'
+export {default as ListaAvatar} from './AvatarList'
 export {default as FormularioComponente} from './FormularioComponente'
 export {default as Header} from './Header'
 export * from './StyledButton'
