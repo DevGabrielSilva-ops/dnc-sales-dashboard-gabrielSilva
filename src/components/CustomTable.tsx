d330d90bd8ac68f86ba312dbd1562dc322f0b778
Gabriel Silva <dev13.gabriel@gmail.com>
2026-04-23 17:24:59 -0300

    feat: criado os componentes de tabela

---
+++ src/components/CustomTable.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { CustomTableProps } from "@/types";
+import { pxParaRem } from "@/utils";
+
+
+
+export const TableWrapper= styled.div`
+    overflow-x: auto;
+    width: 100%;
+    background-color: ${(props) => props.theme.appBackground};
+    table{
+
+        width: 100%;
+        border-collapse: collapse;
+        
+        th,td {
+            height: ${pxParaRem(48)};
+            padding: 0 ${pxParaRem(8)} 0 0;
+            text-align:left;
+            &: last-child {
+                text-align: right;
+                padding: 0;
+            }
+        }
+        
+        th {
+            color: ${(props) => props.theme.typographies.subtitle};
+            font-weight: 600;
+
+        }
+        
+        tr {
+            border-bottom: ${pxParaRem(1)} solid ${(props) => props.theme.appDefaultStroke}; 
+             &: last-child {
+                border-bottom: none;
+            }
+        }
+    
+    }
+`
+
+function CustomTable(props: CustomTableProps) {
+    const {headers ,rows} = props
+    return(
+       <TableWrapper>
+
+        <table>
+            <thead>
+                <tr>
+                    {
+                        headers.map((header,index) => (
+                            <th key={index}>{header}</th>
+                        ))
+                    }
+                </tr>
+            </thead>
+
+            <tbody>
+                {
+                    rows.map((row,rowIndex) => (
+                        <tr key={rowIndex}>
+                            
+                        {
+
+                            row.map((cell,cellIndex) => (
+                                <td key={cellIndex}>
+                                    {cell}
+                                </td>
+                            ) )
+
+                        } 
+                        
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+       </TableWrapper>
+    )
+}
+
+export default CustomTable
